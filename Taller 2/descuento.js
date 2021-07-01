592e8b06704b110f6d759aa9215fbd4e1909bf3c
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-07-01 10:37:11 -0500

    Se finalizó el segundo taller, con la calculador de descuentos con cupones

--- Taller 2/descuento.js
+++ Taller 2/descuento.js
@@ -9,6 +9,57 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 
-
-
 //console.log("El precio original " + precioConDescuento);
+
+//EJERCICIO NORMAL
+/* function buttonPriceDiscount() {
+    const inputPrice = document.getElementById("Inputprice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const Resultp = document.getElementById("Resultp");
+    Resultp.innerText = "El precio con descuento son: $" + precioConDescuento + " dolares";
+} */
+
+
+//RETO
+//Crear un array de cupones
+
+const coupons = [
+    "Sebastian_dev",
+    "Hola_mundo",
+    "Borra_el_lol"
+];
+
+function buttonPriceDiscount() {
+    const inputPrice = document.getElementById("Inputprice");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    let descuento;
+
+    switch (couponValue) {
+        case "Sebastian_dev":
+            descuento = 50;
+            break;
+        case "Hola_mundo":
+            descuento = 35;
+            break;
+        case "Borra_el_lol":
+            descuento = 25;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const Resultp = document.getElementById("Resultp");
+    Resultp.innerText = "El precio con descuento son: $" + precioConDescuento + " dolares";
+}
+
