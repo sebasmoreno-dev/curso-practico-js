f0a6b9159aee375f2eeaa4a89ad4c402f50dea28
Sebastian Moreno <sebastian.moreno.contacto@gmail.com>
2021-07-02 17:41:55 -0500

    Se creo el taller 3, donde trabajamos dos ejercicios: promedio y mediana

--- README.md
+++ README.md
@@ -15,3 +15,10 @@
 - Segundo paso: implementar las formulas en JavaScript
 - Tercer paso: crear funciones
 - Cuarto paso: integrar Js con html
+
+## Taller #3: promedio, mediana y moda.
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las formulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar Js con html
---
+++ Taller 3/mediana.js
@@ -0,0 +1,65 @@
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+
+}
+
+function calcularMediana(lista) {
+
+    const listaOrdenada = lista.sort(function(a, b) {
+        return a - b;
+    });
+
+    const mitadLista = parseInt(listaOrdenada.length / 2);
+
+    function esPar(numero) {
+        if (numero % 2 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    let mediana;
+
+        if (esPar(listaOrdenada.length)) {
+            const elemento1 = listaOrdenada[mitadLista - 1];
+            const elemento2 = listaOrdenada[mitadLista];
+
+            const promedioElemento1Y1 = calcularMediaAritmetica([
+                elemento1,
+                elemento2,
+        ]);
+
+    mediana = promedioElemento1Y1;
+    } else {
+    mediana = listaOrdenada[mitadLista];
+    }
+
+    return mediana;
+}
+console.log(calcularMediana([200, 400, 500, 100, 900]));
+
+
+/* let listax = [2, 5, 7, 8, 9, 22, 13, 1];
+listax.sort(function(a, b) {
+    return a - b;
+});
+console.log(listax); */
+
+
+/* if (lista1 es par?) {
+    necesitamos dos elementos
+    el promedio
+    la mediana
+} else {
+    //posicion de mitadLista1 desntro de lista1
+} */
